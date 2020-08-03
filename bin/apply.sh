ae12d1d3f7364dc9851da18a806937c968ad4eae
James Strachan <james.strachan@gmail.com>
2020-08-03 11:46:26 +0100

    fix: lets merge the terraform output to source

--- bin/apply.sh
+++ bin/apply.sh
@@ -6,5 +6,7 @@ terraform init $TERRAFORM_INPUT
 terraform apply $TERRAFORM_INPUT $TERRAFORM_APPROVE
 
 # now lets update the requirements...
-terraform output jx_requirements > jx-requirements.yml
+terraform output jx_requirements > /tmp/jx-requirements.yml
+jx gitops requirements merge -f /tmp/jx-requirements.yml
+
 
