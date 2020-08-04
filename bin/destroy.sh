1f30907b7080e2341b7f97b1fac983e528bc2c7a
James Strachan <james.strachan@gmail.com>
2020-08-04 10:13:28 +0100

    fix: add better destroy script

--- bin/destroy.sh
+++ bin/destroy.sh
@@ -2,5 +2,29 @@
 
 set -e
 
+# we are about to remove the kubernetes cluster anyway so lets avoid having terraform try and remove k8s resources
+terraform state list | grep .kubernetes_service_account. | while read line
+do
+if [ -z "$line" ]
+then
+      echo "ignoring empty line"
+else
+      echo "removing terraform state of $line"
+      terraform state rm $line
+fi
+done
+
+terraform state list | grep .kubernetes_namespace. | while read line
+do
+if [ -z "$line" ]
+then
+      echo "ignoring empty line"
+else
+      echo "removing terraform state of $line"
+      terraform state rm $line
+fi
+done
+
+
 terraform destroy $TERRAFORM_APPROVE
 
