c20a3dcdc22528e508ddddfcd2d12f7e6c41d68c
James Strachan <james.strachan@gmail.com>
2020-08-06 07:13:26 +0100

    fix: add init when destroying from clean clone

--- bin/destroy.sh
+++ bin/destroy.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# in case we are in a clean git clone...
+terraform init $TERRAFORM_INPUT
+
 # we are about to remove the kubernetes cluster anyway so lets avoid having terraform try and remove k8s resources
 terraform state list | grep .kubernetes_service_account. | while read line
 do
