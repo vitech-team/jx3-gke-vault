0e388b0c2cbe8c51bab65c1218171b1fa720f520
James Rawlings <rawlingsj80@gmail.com>
2020-08-12 16:34:37 +0100

    not needed now as git operator does the merge

--- bin/apply.sh
+++ bin/apply.sh
@@ -5,8 +5,3 @@ set -e
 terraform init $TERRAFORM_INPUT
 terraform apply $TERRAFORM_INPUT $TERRAFORM_APPROVE
 
-# now lets update the requirements...
-terraform output jx_requirements > /tmp/jx-requirements.yml
-jx gitops requirements merge -f /tmp/jx-requirements.yml
-
-
